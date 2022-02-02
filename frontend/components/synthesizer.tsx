7a12dbbeac690950861f1812efea1353ab2be835
amirgamil <amir.amgad01@gmail.com>
2022-02-02 00:56:50 -0500

    wip

--- frontend/components/synthesizer.tsx
+++ frontend/components/synthesizer.tsx
@@ -2,8 +2,10 @@ import { Song, Track, Instrument, StepNoteType, StepType, MidiNote } from "react
 import * as React from "react";
 import styled from "styled-components";
 import { Textarea } from "./textarea";
-import { getNoteFromLetter, memoizedNoteIndices, sortedLetters } from "../lib/musicMappings";
+import { getNoteFromLetter, lettersToNotesMap, memoizedNoteIndices, sortedLetters } from "../lib/musicMappings";
 import { mapRawMusicToSteps, Notes } from "../lib/musicHelpers";
+import { useInterval } from "../hooks/useInterval";
+import { cloneDeep } from "lodash";
 
 //FIXME: not ideal, copied from the reactronic types
 type NoteType = {
@@ -31,6 +33,18 @@ interface Props {
     isLoading: boolean;
 }
 
+interface MusicalShape {
+    xCoord: number;
+    yCoord: number;
+    shape: string;
+    //assume all rectangles for now
+    width: number;
+    hide: boolean;
+    height: number;
+    //time before fading
+    delta: number;
+}
+type callback = () => void;
 export const Synthesizer: React.FC<Props> = ({
     bpm,
     updateSongCallback,
@@ -42,7 +56,8 @@ export const Synthesizer: React.FC<Props> = ({
     const [lastNote, setLastNote] = React.useState<NoteType[]>([]);
     const [resetFullTune, setResetFullTune] = React.useState<boolean>(false);
     const [rawMusic, setRawMusic] = React.useState<string>(rawNotes);
-    const canvasRef = React.useRef<HTMLCanvasElement>();
+    const canvasRef = React.useRef<HTMLCanvasElement>(null);
+    const canvasElements = React.useRef<MusicalShape[]>([]);
 
     React.useEffect(() => {
         if (resetFullTune) {
@@ -53,7 +68,9 @@ export const Synthesizer: React.FC<Props> = ({
                 clearTimeout(restart);
             };
         }
-    }, [resetFullTune]);
+    }, [resetFullTune, rawMusic]);
+
+    const updateCanvasDrawing = () => {};
 
     const convertBackground = (note: string) => {
         const changeBackgroundEvery = bpm / 60;
@@ -104,42 +121,52 @@ export const Synthesizer: React.FC<Props> = ({
                     volume={0}
                     pan={0}
                     mute={false}
-                    onStepPlay={(notes, index) => {
-                        //assuming only one layer notes for now
-                        if (notes.length !== 0) {
-                            const currNote = notes[0].name;
+                    onStepPlay={(note, index) => {
+                        if (note.length !== 0) {
+                            const currNote = note[0].name;
+                            if (currNote) {
+                                //note compromised of two unique things, tone and pitch
+                                //use those two variables to generate a corresponding shape at a unique position
 
-                            const orderIndex = memoizedNoteIndices.get(currNote);
-                            if (canvasRef.current) {
+                                const note = currNote.charAt(0);
+                                const pitch = parseInt(currNote.charAt(1));
+
+                                const xCoord = (pitch / 7) * 500;
+                                const yCoord =
+                                    ((note.toLowerCase().charCodeAt(0) - "a".toLowerCase().charCodeAt(0)) / 8) * 500;
+
+                                if (!canvasRef.current) return;
                                 const ctx = canvasRef.current.getContext("2d");
-                                if (currNote && ctx) {
-                                    //assumes no drum sounds?
-
-                                    //note compromised of two unique things, tone and pitch
-                                    //use those two variables to generate a corresponding shape at a unique position
-                                    const note = currNote.charAt(0);
-                                    const pitch = parseInt(currNote.charAt(1));
-
-                                    const xCoord = (pitch / 7) * window.innerWidth;
-                                    const yCoord =
-                                        ((note.toLowerCase().charCodeAt(0) - "a".toLowerCase().charCodeAt(0)) / 8) *
-                                            (window.innerHeight - 450) +
-                                        450;
-
-                                    ctx.beginPath();
-                                    ctx.beginPath();
-                                    ctx.strokeStyle = "black";
-                                    ctx.rect(xCoord, yCoord, 50, 50);
-                                    ctx.stroke();
+                                if (!ctx) return;
+                                ctx.clearRect(shape.xCoord - 1, shape.yCoord - 1, shape.width + 2, shape.height + 2);
+                                ctx.beginPath();
+                                ctx.fillStyle = "black";
+                                ctx.fillRect(shape.xCoord, shape.yCoord, shape.width, shape.height);
+                                ctx.stroke();
+                                shape.hide = true;
+                                if (index < canvasElements.current.length) {
+                                    canvasElements.current[index].hide = false;
+                                } else {
+                                    canvasElements.current.push({
+                                        xCoord,
+                                        yCoord,
+                                        shape: "rectangle",
+                                        width: 50,
+                                        height: 50,
+                                        delta: 0,
+                                        hide: false,
+                                    });
                                 }
                             }
                         }
+                        console.log(canvasElements.current.length);
+                        updateCanvasDrawing();
                     }}
                 >
                     <Instrument type="synth" oscillator={{ type: "sine" }} />
                 </Track>
             </Song>
-            <canvas ref={canvasRef}></canvas>
+            <canvas width={500} height={500} ref={canvasRef}></canvas>
         </Container>
     );
 };
